1be77f5b34f842ab633075cd3c45485dc4d92676
chrismarquez <christophersmarquez@outlook.com>
2024-12-03 12:52:09 +0000

    init: start project

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm") version "2.1.0"
+    application
+    id("org.graalvm.buildtools.native") version "0.10.3"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "org.voidsynchron"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "org.voidsynchron.MainKt"
+}
+
+shadow {
+    base {
+        archivesName = "cron-cli"
+        version = ""
+    }
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier = ""
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            imageName.set("cron-cli")
+        }
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.github.ajalt.clikt:clikt:5.0.1")
+    implementation("com.github.ajalt.clikt:clikt-markdown:5.0.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
